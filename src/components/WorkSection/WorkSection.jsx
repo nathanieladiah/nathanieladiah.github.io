3dad347d1188edc00c96b426401fb734e5df2fcb
nathanieladiah <nathanieladiah@gmail.com>
2023-10-27 19:02:48 -0400

    Start on work section and cards

--- src/components/WorkSection/WorkSection.jsx
+++ src/components/WorkSection/WorkSection.jsx
@@ -1,6 +1,22 @@
+import ServiceCard from "../ServiceCard/ServiceCard";
 import "./workSection.scss";
 
 const WorkSection = () => {
-  return <section className="work">This is the work section</section>;
+  return (
+    <section className="work container">
+      <div className="section section--services">
+        <h2 className="section__header">Services</h2>
+        <h3 className="section__subheader">
+          What <span className="accent">I do</span>.
+        </h3>
+        <div className="services">
+          {/* <div className="services__card services__card--highlight"></div>
+          <div className="services__card"></div>
+          <div className="services__card"></div> */}
+          <ServiceCard />
+        </div>
+      </div>
+    </section>
+  );
 };
 export default WorkSection;
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import "@fontsource/inter";
 import "@fontsource/kurale";
+import "@fontsource/lato";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
